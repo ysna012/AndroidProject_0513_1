971c1d21e83c2e2f349ba7030fd9f39d89890819
ysna012 <yeasun2970@gmail.com>
2022-05-13 15:18:50 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/yeonsung/ysna/androidproject_0513_1/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.yeonsung.ysna.androidproject_0513_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
